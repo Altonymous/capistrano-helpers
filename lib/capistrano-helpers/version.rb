5b09bde07c390f1d12687cbe78f74d6104510238
Chris Altman <chris@altonymous.com>
2013-02-25 11:24:33 -0500

    Fixes to some cowboy changes
    Made changes directly on github and didn't completely review them
    before saving.

--- lib/capistrano-helpers/version.rb
+++ lib/capistrano-helpers/version.rb
@@ -2,18 +2,19 @@ require File.dirname(__FILE__) + '/../capistrano-helpers' if ! defined?(Capistra
 require File.dirname(__FILE__) + '/branch'
 
 CapistranoHelpers.with_configuration do
- 
+
   namespace :deploy do
     desc "Write the name of the tag that we're deploying to a VERSION file"
     task :write_version_file do
       run "echo -n \"#{branch}\" > #{release_path}/VERSION"
-      latest_commit_sha = `git log origin/1045_add_video_link | head -1`
+
+      latest_commit_sha = `git log origin/#{branch} | head -1`
       run "echo -n \"#{latest_commit_sha}\" > #{release_path}/VERSION"
     end
 
     desc "Get the current version deployed by reading the remote VERSION file"
     task :current_version_deployed, roles: :app do
-      run "cat #release_path}/VERSION"
+      run "cat #{release_path}/VERSION"
     end
   end
 
